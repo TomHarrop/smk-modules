74d8d5bd803d0c29bcc9486f97bb70cda80d35f3
Thomas Harrop <thomas.harrop@csiro.au>
2024-06-20 09:42:52 +1000

    exclude groups with <3 seqs from tree

--- modules/iqtree/scripts/generate_nexus_partition_file.py
+++ modules/iqtree/scripts/generate_nexus_partition_file.py
@@ -1,19 +1,30 @@
 #!/usr/bin/env python3
 
+from Bio import SeqIO
 from pathlib import Path
 import os
 import sys
 
 
-def create_nexus_partition(directory, outfile):
+def count_fasta_records(fasta_file):
+    return len(SeqIO.index(fasta_file, "fasta"))
+
+
+def create_nexus_partition(directory, outfile, reportfile):
     partition_content = []
     file_list = sorted(os.listdir(directory))
 
-    for filename in file_list:
-        if not filename.startswith("."):
-            alignment_name = os.path.splitext(filename)[0]
-            filepath = os.path.join(directory, filename)
-            partition_content.append(f"charset {alignment_name} = {filepath}: *;")
+    with open(reportfile, "a") as f:
+        for filename in file_list:
+            if not filename.startswith("."):
+                alignment_name = os.path.splitext(filename)[0]
+                filepath = os.path.join(directory, filename)
+                if count_fasta_records(filepath) < 3:
+                    f.write(f"{filepath}\n")
+                else:
+                    partition_content.append(
+                        f"charset {alignment_name} = {filepath}: *;"
+                    )
 
     nexus_content = "#NEXUS\n\nbegin sets;\n"
 
@@ -29,6 +40,7 @@ def create_nexus_partition(directory, outfile):
 if __name__ == "__main__":
     directory_path = Path(snakemake.params["alignment_directory"])
     outfile = snakemake.output["nexus"]
-    create_nexus_partition(directory_path, outfile)
+    reportfile = snakemake.output["report"]
+    create_nexus_partition(directory_path, outfile, reportfile)
     with open(snakemake.output["pathfile"], "wt") as f:
         f.write(directory_path.as_posix())
