9ad1b5fde1a270c39fdcb1513515ab5a5680526f
Thomas Harrop <thomas.harrop@csiro.au>
2024-07-25 16:30:51 +1000

    log what happened during orientation

--- modules/paragone/scripts/orient_sequences.py
+++ modules/paragone/scripts/orient_sequences.py
@@ -34,28 +34,33 @@ def process_fasta(input_fasta):
     logger.info(f"Processing FASTA file {input_fasta}")
     with tempfile.NamedTemporaryFile(delete=False, suffix=".fasta") as tmp_output_fasta:
         logger.info(f"Writing output for {input_fasta} to {tmp_output_fasta.name}")
-        reorient_sequences(input_fasta, tmp_output_fasta.name)
-        return tmp_output_fasta.name, input_fasta.name
+        best_orientations = reorient_sequences(input_fasta, tmp_output_fasta.name)
+        return tmp_output_fasta.name, input_fasta.name, best_orientations
 
 
 def reorient_sequences(input_fasta, output_fasta):
     input_handle = open(input_fasta, "r")
     output_handle = open(output_fasta, "w")
     sequences = SeqIO.parse(input_handle, "fasta")
-    # Get the first sequence as ref and write it to the output
+    # record the orientations
+    best_orientations = {}
+    # Get the first sequence as ref and write it to the output. By definition
+    # this is the forward orientation
     reference_seq = next(sequences)
     SeqIO.write(reference_seq, output_handle, "fasta")
+    best_orientations[reference_seq.id] = "forward"
     for seq_record in sequences:
         best_oriented_seq, best_name = best_orientation(
             reference_seq.seq, seq_record.seq
         )
-        logger.info(f"Found orientation {best_name} for sequence {seq_record.id}")
+        best_orientations[seq_record.id] = best_name
         reoriented_record = SeqRecord(
             best_oriented_seq, id=seq_record.id, description=seq_record.description
         )
         SeqIO.write(reoriented_record, output_handle, "fasta")
     input_handle.close()
     output_handle.close()
+    return best_orientations
 
 
 def main():
@@ -64,24 +69,36 @@ def main():
     logger.logger.addHandler(logger.logfile_handler)
 
     input_directory = snakemake.input[0]
-    output_tarfile = snakemake.output[0]
+    output_tarfile = snakemake.output["tarfile"]
+    output_csv = snakemake.output["csv"]
+    n_threads = snakemake.threads
 
     logger.info(f"Looking for fasta files in {input_directory}")
-
     fasta_files = [
         file
         for file in Path(input_directory).glob("*.fasta")
         if not file.name.startswith(".")
     ]
 
-    with Pool(processes=snakemake.threads) as pool:
+    logger.info(f"Processing {len(fasta_files)} fasta files with {n_threads} processes")
+    with Pool(processes=n_threads) as pool:
         results = pool.map(process_fasta, fasta_files)
 
-    with tarfile.open(output_tarfile, "w") as tar:
-        for tmp_output_fasta, original_name in results:
+    logger.info(
+        f"Finished processing {len(results)} files"
+    )
+    with tarfile.open(output_tarfile, "w") as tar, open(output_csv, "wt") as csv:
+        csv.write("filename,sequence_id,orientation\n")
+        for tmp_output_fasta, original_name, best_orientations in results:
             arcname = original_name
+            logger.info(f"Collecting results for {original_name}")
+            logger.info(f"Adding {tmp_output_fasta} to {output_tarfile}")
             tar.add(tmp_output_fasta, arcname=original_name)
+            logger.info(f"Recording orientations in {output_csv}")
+            for k, v in best_orientations.items():
+                csv.write(f"{original_name},{k},{v}\n")
 
+    logger.info("\nDone")
 
 if __name__ == "__main__":
     main()
